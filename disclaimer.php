e92c2a48b146a45f01a826b4dc6189612c41e24f
Yaroslav Salmin <yaroslavsalmin@Yaroslavs-MBP.home>
2017-05-20 13:51:03 +0100

    Disclaimer

---
+++ disclaimer.php
@@ -0,0 +1,60 @@
+<?php
+include_once "header.php";
+?>
+<div class="container-fluid content-page">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1 wega-sm-padding">
+            <h1>Disclaimer</h1>
+            <div class="part">
+                <h2>No warranties</h2>
+                <p>This website is provided "as is" without any representations or warranties, express or implied. Truxtun Capital SA (Truxtun) makes no representations or warranties in relation to this website or the information and materials provided on this website.</p>
+                <p>Without prejudice to the generality of the foregoing paragraph, Truxtun does not warrant that:</p>
+                <ul class="point">
+                    <li>this website will be constantly available, or available at all; or</li>
+	                <li>the information on this website is complete, true, accurate or non-misleading.</li>
+                </ul>
+                <p>Nothing on this website constitutes, or is meant to constitute, advice of any kind. [If you require advice in relation to any [legal, financial or medical] matter you should consult an appropriate professional.</p>
+            </div>
+            <div class="part">
+                <h2>Limitations of liability</h2>
+                <p>Truxtun shall not be liable to you (whether under the law of contact, the law of torts or otherwise) in relation to the contents of, or use of, or otherwise in connection with this website:</p>
+                <ul class="point">
+                    <li>to the extent that the website is provided free-of-charge, for any direct loss;</li>
+                    <li>for any indirect, special or consequential loss; or</li>
+                    <li>for any business losses, loss of revenue, income, profits or anticipated savings, loss of contracts or business relationships, loss of reputation or goodwill, or loss or corruption of information or data.</li>
+                </ul>
+                <p>These limitations of liability apply even if Truxtun has been expressly advised of the potential loss.</p>
+            </div>
+            <div class="part">
+                <h2>Exceptions</h2>
+                <p>Nothing in this website disclaimer will exclude or limit any warranty implied by law that it would be unlawful to exclude or limit; and nothing in this website disclaimer will exclude or limit Truxtun’s liability in respect of any:</p>
+                <ul class="point">
+                    <li>death or personal injury caused by Truxtun ’s intentional acts;</li>
+                    <li>fraud or fraudulent misrepresentation on the part of Truxtun; or</li>
+                    <li>matter which it would be illegal or unlawful for Truxtun to exclude or limit, or to attempt or purport to exclude or limit, its liability.</li>
+                </ul>
+            </div>
+            <div class="part">
+                <h2>Reasonableness</h2>
+                <p>By using this website, you agree that the exclusions and limitations of liability set out in this website disclaimer are reasonable.</p>
+                <p>If you do not think they are reasonable, you must not use this website.</p>
+            </div>
+            <div class="part">
+                <h2>Other parties</h2>
+                <p>You accept that, as a limited liability entity, Truxtun has an interest in limiting the personal liability of its directors, officers and employees. You agree that you will not bring any claim personally against Truxtun directors, officers or employees in respect of any losses you suffer in connection with the website.</p>
+                <p>Without prejudice to the foregoing paragraph, you agree that the limitations of warranties and liability set out in this website disclaimer will protect Truxtun’s directors, officers, employees, agents, subsidiaries, successors, assigns and sub-contractors as well as Truxtun.</p>
+            </div>
+            <div class="part">
+                <h2>Unenforceable provisions</h2>
+                <p>If any provision of this website disclaimer is, or is found to be, unenforceable under applicable law, that will not affect the enforceability of the other provisions of this website disclaimer.</p>
+            </div>
+            <div class="part">
+                <h2>Applicable Law and Jurisdiction</h2>
+                <p>Your use of this Site is governed by the laws of England and Wales. All disputes arising out of or relating to this website shall be resolved by the Courts of London.</p>
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+include_once "footer.php";
+?>
