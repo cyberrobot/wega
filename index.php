c9e0725fbc056248e56c94e6ecd51f76eb49302b
Yaroslav Salmin <yaroslavsalmin@yaroslavs-mbp.home>
2017-09-17 22:44:54 +0100

    How does it work section

--- index.php
+++ index.php
@@ -28,8 +28,6 @@ include_once "header.php";
                         <div class="col-sm-6">
                             <div class="wega-shop-online">
                                 <h1>Shop safely with <span>Wega Virtual payment Card</span></h1>
-                                <!--<div class="app-store"><img class="img-responsive" src="img/app-store.png" alt="Download from App Store"></div>
-                                <div class="google-play"><img class="img-responsive" src="img/google-play.png" alt="Download from Google Play"></div>-->
                                 <ul class="horizontal-menu wega-align">
                                     <li>
                                         <a href="#wega-virtual-card" class="btn blue scroll-link">Learn More</a>
@@ -443,6 +441,191 @@ online, enjoy exclusive advantages and check your accounts remotely in a simple 
                                 </div>
                             </div>
                             <div class="pane hidden-xs"></div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+        <div data-tabs-component>
+            <section id="how-does-it-work" class="how-does-it-work section" data-tabs-container>
+                <div class="wega-skew">
+                    <div class="wega-bg"></div>
+                </div>
+                <div class="row">
+                    <div class="col-md-10 col-md-offset-1 wega-sm-padding">
+                        <div class="wega-content center-text">
+                            <h1>How does it work</h1>
+                            <div class="wega-image-container">
+                                <img class="img-responsive" src="img/more-than-just-a-bank-card.png" alt="">
+                            </div>
+                            <div class="collapsible-tabs" >
+                                <div class="tabs col-sm-10 col-sm-offset-1">
+                                    <div class="row">
+                                        <div class="tab" data-style="vertical-media-text centered">
+                                            <i class="icon icon-shield"></i>
+                                            <span class="tab-title">Section 1</span>
+                                        </div>
+                                        <div class="tab" data-style="vertical-media-text centered">
+                                            <i class="icon icon-shield"></i>
+                                            <span class="tab-title">Section 2</span>
+                                        </div>
+                                        <div class="tab" data-style="vertical-media-text centered">
+                                            <i class="icon icon-shield"></i>
+                                            <span class="tab-title">Section 3</span>
+                                        </div>
+                                        <div class="tab" data-style="vertical-media-text centered">
+                                            <i class="icon icon-shield"></i>
+                                            <span class="tab-title">Section 4</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="collapsible-content section">
+                <div class="wega-skew">
+                    <div class="wega-bg"></div>
+                </div>
+                <div class="row">
+                    <div class="col-md-10 col-md-offset-1 wega-sm-padding">
+                        <div class="wega-content compensate-footer">
+                            <span class="close" data-target="collapsible-content"></span>
+                            <div class="pane">
+                                <div class="row">
+                                    <div class="col-md-8 col-md-offset-2">
+                                        <div class="col-sm-12">
+                                            <h1>Section 1</h1>
+                                            <div class="wega-image-container">
+                                                <img src="img/icon-cardhand.png" alt="">
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-12">
+                                            <div class="wega-text-container pull-top-40">
+                                                <p>To use acoustic function, press the button bottom right under the Mastercard logo. In case the button does not work properly, contact customer support at <a class="tel" href="tel:+441618831591">+44(0)161 883 1591</a></p>
+                                                <p>Wega 3DSA 2.0 card is a regular Mastercard payment card with EMV functionalities and embedded NFC (Near Field Communication) functionalities. It features particularly on top a button, a loudspeaker and a battery that powers unique and patented acoustic functionality.</p>
+
+                                                <h3>How an acoustic payment card works?</h3>
+                                                <p>When the holder presses the card button, it emits a unique digital tone. This acoustic signal is a serial dynamic OTP (One Time Password) which strongly authenticates the Wega 3DSA 2.0 smart card to the authentication servers.
+The authentication server demodulates the acoustic signal to check card integrity and validity, then asks for users additional authentication factors (PIN code or Biometrics), also called strong multi-factor authentication.
+Once signature and additional password (s) are verified by the authentication server, access to the specific services is granted.  
+This process certifies to the bank that the shown card is valid and that the person processing the operation is truly the card holder. This represents the new legal requirement for Electronic and Mobile Commerce known as Strong Customer Authentication according to PSD2.</p>
+<h3>What is the PSD2?</h3>
+<p>Directive 2007/64/CE (hereinafter "PSD") 1, as it is known, regulated the information requirements, the rights and the obligations of payment services users, as well as the prudential requirements for entering the market of entities qualified to provide these services (payment service providers or "PSP"). The establishment of uniform rules for the provision of payment services, has encouraged the creation of a EU internal market for payments. 
+The Directive (EU) 2015/2366 (hereinafter “PSD 2” or the “new Directive”) represents a step further towards complete harmonisation of the EU payments market.This introduces new regulations and standards both to the European existing payment services regime and the authentication landscape.</p>
+<p><strong>Article 97</strong> of the Directive establishes a strong customer authentication (SCA) procedure that applies when the payer:</br>
+- access their online payment account</br>
+- initiates an electronic payment transaction;</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="pane">
+                                <div class="row">
+                                    <div class="col-md-8 col-md-offset-2">
+                                        <div class="col-sm-12">
+                                            <h1>Section 2</h1>
+                                        </div>
+                                        <div class="col-sm-12">
+                                            <div class="wega-text-container pull-top-40">
+                                            <h3>Strong Acoustic Authentication</h3>
+                                            <p>One Outcome to the lack of user authentication in electronic commerce is a greater incidence of payment card fraud. A notable attempt by the payment card industry to reduce this problem is to secure electronic operations. Wega strong objective is to meet this challenge while fully comply with PSD2</p>
+                                            
+                                            <h3>Identifying with Wega</h3>
+                                            <p>While needing to access any account functionalities , the user choses to place its Wega card in front of the microphone and presses the button on the card. An audio sequence is issued by the card and then transmitted to the interactive server. This first step is used to identify the user and the holder of the card. No EMV related information are passed during this sequence. There are no technical relationship between the acoustic Chip and the EMV elements. The audio sequence only contains dynamic OTPs (One-Time-Password) and a unique token reference.</p>
+                                            
+                                            <h3>Authenticating with Wega</h3>
+                                            <p>Once the user has been identified, the server asks the user to confirm their authentication by entering another biometric FIDO certified element and the PIN. When these are validated by the 3DSA 2.0 Server FIDO certified , the secured access to the operation or services is granted.
+                                            Thus, the acoustic chip is only used for internal account validation and confirmation of card ordering, mobile top up, card to card transfers, single use cards and access securely to Facebox personal safe. It is a dynamic token with an OTP replacing username and password as defined in the FIDO UAF specifications.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="pane">
+                                <div class="row">
+                                    <div class="col-md-8 col-md-offset-2">
+                                        <div class="col-sm-12">
+                                            <h1>Section 2</h1>
+                                        </div>
+                                        <div class="col-sm-12">
+                                            <div class="wega-text-container pull-top-40">
+                                            <h3>About FIDO Alliance and FIDO standards</h3>
+                                            <p>The Fast Identity Online (FIDO) Alliance was formed in 2013 to revolutionize online authentication by developing open, interoperable industry standards that leverage proven public key cryptography for stronger security and device-based user verification for better usability. Through the collaborative efforts of FIDO, new standards and specifications have emerged that enable strong, easy-to-use authentication to be built into devices.</br>
+In a typical deployment of these standards, a user swipes a finger, speaks a phrase, looks at a camera on a device to login, pay for an item, or use another service.</br>
+The biometric verification is used as an initial factor to then unlock a second, more secure factor. Since biometrics and cryptographic keys are stored on local devices and never sent across the network – eliminating shared secrets – user credentials are secure even if service providers get hacked, thereby eliminating the possibility of scalable data breaches.</br> 
+FIDO standards are currently being used to enable simpler, stronger authentication in offerings from Google, PayPal, Bank of America, NTT DOCOMO, BC Card (Korea), Microsoft, Dropbox, GitHub, AliPay, eBay, Samsung, Facebook, and other leading firms. In each of these deployments, end-users do not have to know how the authentication works or why it’s more secure – they are getting login experiences that are easier to use, with great security baked in behind the scenes.</br>
+The FIDO approach has been embraced by the World Wide Web Consortium (W3C), which is expected to finalize a formal new “Web Authentication” standard built on FIDO specifications in 2017. </p>
+<p>FIDO Alliance was selected by EMVCo for the use of FIDO concepts and requirements in EMVCo 3DS 2.0 specifications for multi-factor Strong Authentication solutions with the following objectives:</p>
+
+<p>
+- Reduce card fraud</br>
+- Maintain convenient user experience</br>
+- Greatly simplify the development and support for CDCVM across mobile devices and other platforms</br>
+- Provide a standard way for mobile wallet providers and payment application developers to support Consumer Device Cardholder Verification Methods (CDCVM)</p>
+<h3>FIDO Certified</h3>
+<p>This ecosystem enables enterprises and service providers to deploy strong authentication solutions that reduce reliance on passwords and protect against phishing, man-in-the-middle and replay attacks using stolen passwords.</br>
+As such, Wega’s acoustic chip, 3DSA 2.0 servers and Pin Code have all been <a href="https://fidoalliance.org/certification/fido-certified-products/">FIDO Certified</a>
+Fido ignites a thriving ecosystem of client authentication methods such as biometrics, PINs and second–factors that can be used with a variety of online services in an interoperable manner. To complete its operating process, Wega has selected qualified FIDO certified biometrics authenticators in the 3DSA 2.0 platform and has integrated then within its plateform and its Wega mobile app.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="pane">
+                                <div class="row">
+                                    <div class="col-md-8 col-md-offset-2">
+                                        <div class="col-sm-12">
+                                            <h1>Section 4</h1>
+                                        </div>
+                                        <div class="col-sm-12">
+                                            <div class="wega-text-container pull-top-40">
+                                            <h3>How to activate my Wega card</h3>
+                                            <p>In order to start using the card please undertake the following simple and secure steps:
+Sign it at the back and activate it as follows:</p>
+<p><ul class="bullet">
+    <li>Via mobile application Manchester City Wega</br>
+You have downloaded the Manchester City Wega mobile app and registered your Wega 3DSA account on the app:
+        <ul class="bullet">
+            <li>Go to the Activation button.</li>    
+            <li>Fill out the CID (The Cardholder ID is the 12 digit number printed on the front of you card under the cardholder’s name)
+</li>
+            <li>Fill out your date of birth in the following format DD/MM/YY (plse check)</li>
+            <li>Push the X symbol on the corner of your Wega 3DSA card  close to the microphone of your mobile phone. When the Wega</li>
+            <li>3DSA system recognizes the unique acoustic password your card will be then activated.</li>
+
+        </ul>
+        You have not downloaded the Manchester City Wega mobile app available on Google Play and App Store yet. Please do so and go through the Wega 3DSA account registration process. Then please continue with steps a.i.1 through 4 above.
+    </li>
+    <li>Via the website</li>
+    <li>By calling us on our number <a class="tel" href="tel:+441618831501">+44 (0) 161 883 1501</a></li>
+</ul></p>
+<p>Then you will need to provide your card number and DOB in DD/MM/YY format.</p>
+<h3>How to load your Wega card?</h3>
+<p>There are three ways:</p>
+    <ul class="bullet">
+        <li>Via bank transfer</li>
+        <li>Via debit card</li>
+        <li>Via Neosurf voucher</li>
+    </ul>
+<p>Now that you card  is activated and loaded below are some operations you can do securely with your Wega 3DSA 2.0 payment card.</p>
+    <ul class="bullet">
+        <li>You can pay for goods and services now at millions of locations worldwide instead of cash and checks and at any ATM that displays the Mastercard acceptance mark.</li>
+        <li>You retain full cost control of the money you spend.</li>
+        <li>Apply for extra cards for you partner relatives or business associates.</li>
+        <li>Check your balance or review a list of transactions on your mobile app, via the website.</li>
+        <li>Accept payments whenever and wherever you want, via bank transfer from other accounts or from other cardholders.</li>
+        <li>Share money with another cardholder. This replaces many other, more expensive money transfer methods you may have been using previously.</li>
+    </ul>
+    <p>You have access to Facebox, your digital safe to store all your confidential documents</p>
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
